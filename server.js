81727b0c8cb7ca382d0c7b4168122ad1643bb8fe
iwona <iwo.jarek@gmail.com>
2022-10-07 11:27:09 +0200

    27.4

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const path = require("path");
 const hbs = require("express-handlebars");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 const app = express();
 app.engine("hbs", hbs());
 app.set("view engine", "hbs");
 
 app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.urlencoded({ extended: false }));
+
+app.post("/contact/send-message", upload.single("uploadDesign"), (req, res) => {
+  const { author, sender, title, message } = req.body;
+
+  if (author && sender && title && message && req.file) {
+    res.render("contact", { isSent: true, fileName: req.file.originalname });
+  } else {
+    res.render("contact", { isError: true });
+  }
+});
 
 app.get("/", (req, res) => {
   res.render("index");
